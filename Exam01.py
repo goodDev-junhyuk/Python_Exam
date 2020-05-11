ed752d7409840a6277d12b901cbc7cbc21262c7e
junhyuk <dejavukk1@gmail.com>
2020-05-11 16:01:57 +0900

    두번째 커밋.

--- Exam01.py
+++ Exam01.py
@@ -1,2 +1,12 @@
 print("Hello Python!!")
 print(1+1)
+
+result = 10
+print(result)
+
+result = 20
+print(result)
+
+print("10")
+print("20")
+
---
+++ Exam02.py
@@ -0,0 +1,6 @@
+password = 1234
+# 분기문.
+if password == 1234:
+    print("문을 열어준다.")
+else:
+    print("아무것도 하지 않는다.")
