e9af1c0aa1e4303dceb16dd3f9c88d8064ae1957
junhyuk <dejavukk1@gmail.com>
2020-05-11 17:02:14 +0900

    두번째 커밋.

---
+++ Exam03.py
@@ -0,0 +1,20 @@
+# 함수, 메소드 Parameter.
+
+def print_hello():
+    print("hello")
+
+print_hello()
+
+def print_message(p_message):
+    print(p_message)
+
+print_message("bye")
+
+def pikachu(skill_number):
+    if skill_number == 1:
+        print("전기공격")
+    elif skill_number == 2:
+        print("몸통박치기!!")
+
+pikachu(2)
+
