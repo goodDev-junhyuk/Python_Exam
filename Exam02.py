108dd4f86cd45221ac033a83217e22c88d6fc272
junhyuk <dejavukk1@gmail.com>
2020-05-11 16:40:44 +0900

    두번째 커밋.

--- Exam02.py
+++ Exam02.py
@@ -4,3 +4,15 @@ if password == 1234:
     print("문을 열어준다.")
 else:
     print("아무것도 하지 않는다.")
+
+
+score = 77
+
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+else:
+    print("F")
